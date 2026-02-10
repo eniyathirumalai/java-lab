8432ac098e07deb670c4f3e78e8d89c46150b544
eniyathirumalai <eniyathirumalai7@gmail.com>
2026-02-10 03:00:50 -0800

    Add files via upload

---
+++ practice/Add.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+    class Add{
+      public static void main(String arr[]){
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("Enter the first number:");
+        int a=sc.nextInt();
+
+        System.out.print("Enter second number:");
+        int b=sc.nextInt();
+        int sum=a+b;
+        System.out.println("Sum="+sum);
+}
+}                                                                                                                      
+	
